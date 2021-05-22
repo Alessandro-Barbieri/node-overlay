bb791819e02bf19eb659e6b2fd0ce03ef94a9276
Alessandro-Barbieri <lssndrbarbieri@gmail.com>
2021-05-22 18:36:46 +0200

    fix.sh: optimize sedding
    Signed-off-by: Alessandro-Barbieri <lssndrbarbieri@gmail.com>

--- fix.sh
+++ fix.sh
@@ -2,18 +2,19 @@
 
 set -xe
 
-find . -name "*.ebuild" -type f -print0 | xargs -0 sed -i -e 's|BSD-2-Clause|BSD-2|g'
-find . -name "*.ebuild" -type f -print0 | xargs -0 sed -i -e 's|BSD-3-Clause|BSD|g'
-find . -name "*.ebuild" -type f -print0 | xargs -0 sed -i -e 's|dev-node/babel|node-babel/babel|g'
-find . -name "*.ebuild" -type f -print0 | xargs -0 sed -i -e 's|dev-node/eslint|node-eslint/eslint|g'
-find . -name "*.ebuild" -type f -print0 | xargs -0 sed -i -e 's|dev-node/lodash|node-lodash/lodash|g'
-find . -name "*.ebuild" -type f -print0 | xargs -0 sed -i -e 's|node-lodash/lodash.|node-lodash/lodash_|g'
-find . -name "*.ebuild" -type f -print0 | xargs -0 sed -i -e 's|dev-node/jest+|node-jest/jest+|g'
-find . -name "*.ebuild" -type f -print0 | xargs -0 sed -i -e 's|dev-node/jest-|node-jest/jest-|g'
-find . -name "*.ebuild" -type f -print0 | xargs -0 sed -i -e 's|dev-node/postcss|node-postcss/postcss|g'
-find . -name "*.ebuild" -type f -print0 | xargs -0 sed -i -e 's|dev-node/rollup|node-rollup/rollup|g'
-find . -name "*.ebuild" -type f -print0 | xargs -0 sed -i -e 's|dev-node/types+|node-types/types+|g'
-find . -name "*.ebuild" -type f -print0 | xargs -0 sed -i -e 's|dev-node/vue|node-vue/vue|g'
+find . -name "*.ebuild" -type f -print0 | xargs -0 sed -i \
+	-e 's|BSD-2-Clause|BSD-2|g' \
+	-e 's|BSD-3-Clause|BSD|g' \
+	-e 's|dev-node/babel|node-babel/babel|g' \
+	-e 's|dev-node/eslint|node-eslint/eslint|g' \
+	-e 's|dev-node/lodash|node-lodash/lodash|g' \
+	-e 's|node-lodash/lodash.|node-lodash/lodash_|g' \
+	-e 's|dev-node/jest+|node-jest/jest+|g' \
+	-e 's|dev-node/jest-|node-jest/jest-|g' \
+	-e 's|dev-node/postcss|node-postcss/postcss|g' \
+	-e 's|dev-node/rollup|node-rollup/rollup|g' \
+	-e 's|dev-node/types+|node-types/types+|g' \
+	-e 's|dev-node/vue|node-vue/vue|g'
 
 find . -name "*.xml" -type f -print0 | xargs -0 xmlstarlet ed --inplace -d '//*[not(./*) and (not(./text()) or normalize-space(./text())="")]'
 
